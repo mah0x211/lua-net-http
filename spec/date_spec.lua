e666ddf5c2de7dbd0bc03e7be8e8f5aed640a3b8
Masatoshi Teruya <mah0x211@gmail.com>
2018-05-24 15:33:01 +0900

    Update .travis.yml and spec/date_spec.lua

--- spec/date_spec.lua
+++ spec/date_spec.lua
@@ -1,4 +1,6 @@
 local date = require('net.http.date')
+local sleep = require('process').sleep
+
 
 describe('test net.http.date', function()
 
@@ -6,10 +8,14 @@ describe('test net.http.date', function()
         local d = date.now()
 
         assert.is_equal( 'string', type(d) )
+
+        d = date.update()
+        assert.is_equal( 'string', type(d) )
     end)
 
     it('returns a cached date string', function()
         local d1 = date.now()
+        sleep(1)
         local d2 = date.now()
 
         assert.is_equal( d1, d2 )
@@ -17,9 +23,14 @@ describe('test net.http.date', function()
 
     it('update a cached date string', function()
         local d1 = date.now()
+        sleep(1)
         local d2 = date.update()
 
+        assert.is_not_nil( d2 )
         assert.is_not_equal( d1, d2 )
+
+        sleep(1)
+        assert.is_equal( d2, date.now() )
     end)
 end)
 
