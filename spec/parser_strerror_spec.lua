7e11dacd06aa1034bf84b6ec9feb312ce6374aee
Masatoshi Teruya <mah0x211@gmail.com>
2018-06-06 19:03:17 +0900

    Update spec/parser_strerror_spec.lua

--- spec/parser_strerror_spec.lua
+++ spec/parser_strerror_spec.lua
@@ -1,10 +1,9 @@
-local Parser = require('net.http.parser')
-local strerror = Parser.strerror
-
+local Parse = require('net.http.parse')
+local strerror = Parse.strerror
 
 describe("test net.http.parser.strerror", function()
     it("returns the message string corresponding to error code", function()
-        for k, errcode in pairs( Parser ) do
+        for k, errcode in pairs( Parse ) do
             if type( k ) == 'string' and type( errcode ) == 'number' then
                 local msg = strerror( errcode )
                 assert.is_equal( 'string', type( msg ) )
@@ -14,9 +13,9 @@ describe("test net.http.parser.strerror", function()
     end)
 
     it("returns the unknown message string", function()
-        local msg = strerror( 1.1 )
+        local msg = strerror( 11 )
         assert.is_equal( 'string', type( msg ) )
-        assert.is_equal( 'unknown error code', msg )
+        assert.is_equal( 'unknown error', msg )
     end)
 end)
 
